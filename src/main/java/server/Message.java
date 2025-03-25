75ceeb8e21b1fff4ba8b1742c5d535403a767f0a
MoizNaeem <moiznaeem20@gmail.com>
2025-03-26 01:20:18 +0200

    Updated to help with new endpoint

--- src/main/java/server/Message.java
+++ src/main/java/server/Message.java
@@ -7,68 +7,87 @@ import java.time.ZonedDateTime;
 import java.util.List;
 
 public class Message {
-    private String recordIdentifier;
-    private String recordDescription;
-    private String recordPayload;
-    private String recordRightAscension;
-    private String recordDeclination;
-    private ZonedDateTime sent;
-    private long recordTimeReceived;
+	private String recordIdentifier;
+	private String recordDescription;
+	private String recordPayload;
+	private String recordRightAscension;
+	private String recordDeclination;
+	private ZonedDateTime sent;
+	private long recordTimeReceived;
 	private String recordOwner;
 	private Observatory observatory;
 	private ObservatoryWeather observatoryWeather;
+	private String updatereason;
+	private String messagePoster;
 
-    public Message(String recordIdentifier, String recordDescription, String recordPayload,
-                   String recordRightAscension, String recordDeclination, ZonedDateTime sent,
-				   String recordOwner, Observatory observatory, ObservatoryWeather observatoryWeather) {
+	public Message(String recordIdentifier, String recordDescription, String recordPayload,
+				   String recordRightAscension, String recordDeclination, ZonedDateTime sent,
+				   String recordOwner, Observatory observatory, ObservatoryWeather observatoryWeather, String updatereason, String messagePoster ) {
 
-        this.recordIdentifier = recordIdentifier;
-        this.recordDescription = recordDescription;
-        this.recordPayload = recordPayload;
-        this.recordRightAscension = recordRightAscension;
-        this.recordDeclination = recordDeclination;
-        this.sent = sent;
+
+		this.recordIdentifier = recordIdentifier;
+		this.recordDescription = recordDescription;
+		this.recordPayload = recordPayload;
+		this.recordRightAscension = recordRightAscension;
+		this.recordDeclination = recordDeclination;
+		this.sent = sent;
 		this.recordTimeReceived = ZonedDateTime.now().toInstant().toEpochMilli();
 		this.recordOwner = recordOwner;
 		this.observatory = observatory;
 		this.observatoryWeather = observatoryWeather;
+		this.updatereason = updatereason;
+		this.messagePoster = messagePoster;
 	}
 
-    public Message(String recordIdentifier, String recordDescription, String recordPayload,
-                   String recordRightAscension, String recordDeclination, long recordTimeReceived,
-				   String recordOwner , Observatory observatory, ObservatoryWeather observatoryWeather) {
-        this.recordIdentifier = recordIdentifier;
-        this.recordDescription = recordDescription;
-        this.recordPayload = recordPayload;
-        this.recordRightAscension = recordRightAscension;
-        this.recordDeclination = recordDeclination;
-        this.recordTimeReceived =  recordTimeReceived;
-        this.sent = ZonedDateTime.ofInstant(Instant.ofEpochMilli(recordTimeReceived), ZoneOffset.UTC);
+	public Message(String recordIdentifier, String recordDescription, String recordPayload,
+				   String recordRightAscension, String recordDeclination, long recordTimeReceived,
+				   String recordOwner , Observatory observatory, ObservatoryWeather observatoryWeather, String updatereason, String messagePoster) {
+		this.recordIdentifier = recordIdentifier;
+		this.recordDescription = recordDescription;
+		this.recordPayload = recordPayload;
+		this.recordRightAscension = recordRightAscension;
+		this.recordDeclination = recordDeclination;
+		this.recordTimeReceived =  recordTimeReceived;
+		this.sent = ZonedDateTime.ofInstant(Instant.ofEpochMilli(recordTimeReceived), ZoneOffset.UTC);
 		this.recordOwner = recordOwner;
 		this.observatory = observatory;
 		this.observatoryWeather = observatoryWeather;
-    }
+		this.updatereason = updatereason;
+		this.messagePoster = messagePoster;
+	}
 
-    public void setRecordTimeReceived (long epoch) {
-        this.recordTimeReceived = epoch;
-        this.sent = ZonedDateTime.ofInstant(Instant.ofEpochMilli(epoch), ZoneOffset.UTC);
-    }
-    public void setRecordTimeReceived(ZonedDateTime sent) {
-        this.sent = sent;
-        this.recordTimeReceived = sent.toInstant().toEpochMilli();
-    }
+	public void setRecordTimeReceived (long epoch) {
+		this.recordTimeReceived = epoch;
+		this.sent = ZonedDateTime.ofInstant(Instant.ofEpochMilli(epoch), ZoneOffset.UTC);
+	}
+	public void setRecordTimeReceived(ZonedDateTime sent) {
+		this.sent = sent;
+		this.recordTimeReceived = sent.toInstant().toEpochMilli();
+	}
 
 	public void setRecordOwner(String recordOwner) {
 		this.recordOwner = recordOwner;
 	}
 
+	public String getUpdatereason() {
+		return updatereason;
+	}
+	public String getMessagePoster() {
+		return messagePoster;
+	}
+	public void setMessagePoster(String messagePoster) {
+		this.messagePoster = messagePoster;
+	}
+	public void setUpdatereason(String updatereason) {
+		this.updatereason = updatereason;
+	}
 	public String getRecordOwner() {
 		return recordOwner;
 	}
 
 	public String getRecordIdentifier() {
-        return recordIdentifier;
-    }
+		return recordIdentifier;
+	}
 
 	public void setObservatory(Observatory observatory) {
 		this.observatory = observatory;
@@ -78,21 +97,21 @@ public class Message {
 		return observatory;
 	}
 
-    public void setRecordIdentifier(String recordIdentifier) {
-        this.recordIdentifier = recordIdentifier;
-    }
+	public void setRecordIdentifier(String recordIdentifier) {
+		this.recordIdentifier = recordIdentifier;
+	}
 
-    public String getRecordDescription() {
-        return recordDescription;
-    }
+	public String getRecordDescription() {
+		return recordDescription;
+	}
 
-    public void setRecordDescription(String recordDescription) {
-        this.recordDescription = recordDescription;
-    }
+	public void setRecordDescription(String recordDescription) {
+		this.recordDescription = recordDescription;
+	}
 
-    public String getRecordPayload() {
-        return recordPayload;
-    }
+	public String getRecordPayload() {
+		return recordPayload;
+	}
 
 	public void setObservatoryWeather(ObservatoryWeather observatoryWeather) {
 		this.observatoryWeather = observatoryWeather;
@@ -103,36 +122,36 @@ public class Message {
 	}
 
 	public void setRecordPayload(String recordPayload) {
-        this.recordPayload = recordPayload;
-    }
+		this.recordPayload = recordPayload;
+	}
 
-    public String getRecordRightAscension() {
-        return recordRightAscension;
-    }
+	public String getRecordRightAscension() {
+		return recordRightAscension;
+	}
 
-    public void setRecordRightAscension(String recordRightAscension) {
-        this.recordRightAscension = recordRightAscension;
-    }
+	public void setRecordRightAscension(String recordRightAscension) {
+		this.recordRightAscension = recordRightAscension;
+	}
 
-    public String getRecordDeclination() {
-        return recordDeclination;
-    }
+	public String getRecordDeclination() {
+		return recordDeclination;
+	}
 
-    public void setRecordDeclination(String recordDeclination) {
-        this.recordDeclination = recordDeclination;
-    }
+	public void setRecordDeclination(String recordDeclination) {
+		this.recordDeclination = recordDeclination;
+	}
 
-    public String getRecordTimeReceived() {
-        return Instant.ofEpochMilli(recordTimeReceived)
-                      .atZone(ZoneOffset.UTC)
-                      .toString();
-    }
+	public String getRecordTimeReceived() {
+		return Instant.ofEpochMilli(recordTimeReceived)
+			.atZone(ZoneOffset.UTC)
+			.toString();
+	}
 
 
 
-    public long dateAsInt() {
-        return sent.toInstant().toEpochMilli();
-    }
+	public long dateAsInt() {
+		return sent.toInstant().toEpochMilli();
+	}
 
 	@Override
 	public String toString() {
